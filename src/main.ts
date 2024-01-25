ed8d7e290dd56fd3caffa65b2b03e1059b8b130f
Erik <erikhau3010@gmail.com>
2024-01-25 07:00:40 +0700

    feat: clean up

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import Zapp from "./elements/Zapp";
 
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
+const setCanvasSize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+window.addEventListener("resize", setCanvasSize);
+setCanvasSize();
+
 new Zapp({ canvas });
